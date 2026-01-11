3b0efde79f6d354302bea00761b8ad056c1ee46c
debapriyo-das24 <deb.11gb@gmail.com>
2026-01-12 01:48:05 +0530

    Add files via upload
    Changing habits

---
+++ day1_basics.py
@@ -0,0 +1,51 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "8e93ecd1-287e-420a-ac70-585e3341626d",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Showing up despite disruptions and inertia\n"
+     ]
+    }
+   ],
+   "source": [
+    "print(\"Showing up despite disruptions and inertia\")\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "1a4587ea-7d4e-45d8-b4b8-e27512e78dfb",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python [conda env:base] *",
+   "language": "python",
+   "name": "conda-base-py"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.12.7"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
